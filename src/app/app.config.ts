770038e2e6f36e5238256ab9b4f9c4cd0e72be71
raulshred <raulshred@hotmail.com>
2026-01-23 01:02:30 +0100

    Prepare translations system.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { SiteConfigService } from './services/site-config/site-config.service';
 import { firstValueFrom } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TRANSLATE_HTTP_LOADER_CONFIG, TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// export function httpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http);
+// }
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +23,29 @@ export const appConfig: ApplicationConfig = {
       deps: [SiteConfigService],
       useFactory: (svc: SiteConfigService) => () => firstValueFrom(svc.load()),
     },
-  ]
-};
+    provideHttpClient(),
+
+    provideTranslateService({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useClass: TranslateHttpLoader,
+      },
+    }),
+    {
+      provide: TRANSLATE_HTTP_LOADER_CONFIG,
+      useValue: {
+        prefix: '/assets/i18n/',
+        suffix: '.json',
+      },
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [TranslateService],
+      useFactory: (ts: TranslateService) => () => {
+        ts.setDefaultLang('es');
+        return firstValueFrom(ts.use('es')); // <-- dispara GET /assets/i18n/es.json
+      },
+    },
+  ]};
