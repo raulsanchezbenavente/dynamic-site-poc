9fac22dfbeb14509799413743d4e35d2fc408582
raulshred <raulshred@hotmail.com>
2026-02-23 12:05:23 +0100

    Add lazy loading.

--- src/app/dynamic-composite/dynamic-page/block-outlet.component.ts
+++ src/app/dynamic-composite/dynamic-page/block-outlet.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input, signal, Type } from '@angular/core';
 
 import { componentMap } from '../../component-map';
+
+const componentCache = new Map<string, Type<any>>();
 
 @Component({
   selector: 'block-outlet',
@@ -10,6 +12,10 @@ import { componentMap } from '../../component-map';
   template: `
     @if (cmp(); as c) {
       <ng-container *ngComponentOutlet="c; inputs: inputs()"></ng-container>
+    } @else if (isLoading()) {
+      <div style="padding:.5rem;border:1px dashed #bbb;background:#fff;margin-right: auto;margin-left: auto;">
+        Loading component: <b>{{ block()?.component }}</b>
+      </div>
     } @else {
       <div style="padding:.5rem;border:1px dashed #bbb;background:#fff;margin-right: auto;margin-left: auto;">
         Missing component: <b>{{ block()?.component }}</b>
@@ -21,10 +27,62 @@ import { componentMap } from '../../component-map';
 export class BlockOutletComponent {
   public block = input<any | null | undefined>(undefined);
 
-  public cmp = computed(() => {
-    const b = this.block();
-    return b?.component ? componentMap[b.component] : undefined;
-  });
+  public cmp = signal<Type<any> | undefined>(undefined);
+  public isLoading = signal(false);
+  public isMissing = signal(false);
+
+  private loadId = 0;
+
+  constructor() {
+    effect(() => {
+      const block = this.block();
+      const key = block?.component;
+      const loadId = ++this.loadId;
+
+      if (!key) {
+        this.cmp.set(undefined);
+        this.isLoading.set(false);
+        this.isMissing.set(false);
+        return;
+      }
+
+      const cached = componentCache.get(key);
+      if (cached) {
+        this.cmp.set(cached);
+        this.isLoading.set(false);
+        this.isMissing.set(false);
+        return;
+      }
+
+      const loader = componentMap[key];
+      if (!loader) {
+        this.cmp.set(undefined);
+        this.isLoading.set(false);
+        this.isMissing.set(true);
+        return;
+      }
+
+      this.cmp.set(undefined);
+      this.isLoading.set(true);
+      this.isMissing.set(false);
+
+      loader()
+        .then((component) => {
+          if (this.loadId !== loadId) return;
+          componentCache.set(key, component);
+          this.cmp.set(component);
+        })
+        .catch(() => {
+          if (this.loadId !== loadId) return;
+          this.cmp.set(undefined);
+          this.isMissing.set(true);
+        })
+        .finally(() => {
+          if (this.loadId !== loadId) return;
+          this.isLoading.set(false);
+        });
+    });
+  }
 
   public inputs = computed<Record<string, any>>(() => {
     const b = this.block();
