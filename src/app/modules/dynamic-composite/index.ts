c7ed7b9e270766ae77754c10e9f690e7be089663
Raúl Sánchez <173062928+raulsanchezbenavente@users.noreply.github.com>
2026-03-28 10:27:46 +0100

    Moved to first level.

--- src/app/modules/dynamic-composite/index.ts
+++ src/app/modules/dynamic-composite/index.ts
@@ -1,8 +1,8 @@
 export { BlockOutletComponent } from './block-outlet/block-outlet.component';
-export { DynamicPageReadinessBase } from './core-blocks/dynamic-page-readiness/dynamic-page-readiness.base';
-export type { DynamicPageReadyState } from './core-blocks/dynamic-page-readiness/dynamic-page-readiness.base';
 export { RteInjectorComponent } from './core-blocks/rte-injector/rte-injector.component';
 export { DynamicBlocksComponent } from './dynamic-blocks/dynamic-blocks.component';
+export { DynamicPageReadinessBase } from './dynamic-page-readiness/dynamic-page-readiness.base';
+export type { DynamicPageReadyState } from './dynamic-page-readiness/dynamic-page-readiness.base';
 export { DynamicPageComponent } from './dynamic-page/dynamic-page.component';
 export { DsTabsComponent } from './dynamic-tabs/tabs.component';
 
