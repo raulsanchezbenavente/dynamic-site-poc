37a018e61bf9a32fbe446513f6ec9e120b4b03bb
raulshred <raulshred@hotmail.com>
2026-03-30 14:44:06 +0200

    Fix error in ds summary.

--- src/app/modules/design-system/src/lib/components/summary/compat/ds-summary-builder.component.ts
+++ src/app/modules/design-system/src/lib/components/summary/compat/ds-summary-builder.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import type { SummaryDataRenderer } from '../summary-renderer/models/summary-data-renderer.model';
 
 @Component({
   selector: 'ds-summary-builder',
   standalone: true,
   template: `<ng-content></ng-content>`,
 })
-export class DsSummaryBuilderComponent {}
+export class DsSummaryBuilderComponent {
+  @Input() public config: Record<string, SummaryDataRenderer> = {};
+  @Output() public configChange = new EventEmitter<Record<string, SummaryDataRenderer>>();
+}
--- src/app/modules/design-system/src/lib/components/summary/compat/ds-summary-renderer.component.ts
+++ src/app/modules/design-system/src/lib/components/summary/compat/ds-summary-renderer.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import type { SummaryDataRenderer } from '../summary-renderer/models/summary-data-renderer.model';
 
 @Component({
   selector: 'ds-summary-renderer',
   standalone: true,
   template: `<ng-content></ng-content>`,
 })
-export class DsSummaryRendererComponent {}
+export class DsSummaryRendererComponent {
+  @Input() public config: Record<string, SummaryDataRenderer> = {};
+}
