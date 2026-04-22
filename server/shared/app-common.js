99a390adc2a7678061a7d4a9fd18f590691f2764
raulshred <raulshred@hotmail.com>
2026-04-22 15:35:32 +0200

    Correct redirection to dev3 env.

--- server/shared/app-common.js
+++ server/shared/app-common.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('node:fs');
+const https = require('node:https');
 const express = require('express');
 const { createFakeApiRouter } = require('../fake-api/router');
 
@@ -13,6 +14,45 @@ function mountSharedRoutes(app, options = {}) {
 
   app.get(healthCheckPath, (_req, res) => {
     res.status(200).type('text/plain').send('ok');
+  });
+
+  app.use('/static-config', (req, res) => {
+    const targetUrl = `https://av-static-dev3.newshore.es/static-config${req.url}`;
+    console.log(`[Static Config Proxy] ${req.method} /static-config${req.url} -> ${targetUrl}`);
+
+    const upstreamReq = https.request(
+      targetUrl,
+      {
+        method: req.method,
+        headers: {
+          ...req.headers,
+          host: 'av-static-dev3.newshore.es',
+        },
+      },
+      (upstreamRes) => {
+        res.status(upstreamRes.statusCode || 502);
+
+        Object.entries(upstreamRes.headers).forEach(([key, value]) => {
+          if (value !== undefined) {
+            res.setHeader(key, value);
+          }
+        });
+
+        upstreamRes.pipe(res);
+      }
+    );
+
+    upstreamReq.on('error', (error) => {
+      console.error('[Static Config Proxy] upstream error:', error?.message || error);
+      if (!res.headersSent) {
+        res.status(502).json({
+          error: 'static_config_upstream_unreachable',
+          message: 'Could not retrieve static-config from av-static-dev3.newshore.es',
+        });
+      }
+    });
+
+    req.pipe(upstreamReq);
   });
 
   if (countriesFlagsDir && fs.existsSync(countriesFlagsDir)) {
